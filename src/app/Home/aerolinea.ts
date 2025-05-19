af1f290b84ab301c7b3f70d77162eda2fec81702
Felipe <f.sanmiguel@uniandes.edu.co>
2025-05-19 15:56:21 -0500

    Clases

---
+++ src/app/Home/aerolinea.ts
@@ -0,0 +1,33 @@
+import { Vuelo } from "./vuelo";
+
+
+export class Aerolinea {
+    id: string;
+    airlineName: string;
+    country: string;
+    city: string;
+    address: string;
+    identityColor: string;
+    slogan: string;
+    flights: Vuelo[];
+
+  
+    constructor(id: string,
+        airlineName: string,
+        country: string,
+        city: string,
+        address: string,
+        identityColor: string,
+        slogan: string,
+        flights: Vuelo[]) {
+
+      this.id = id;
+      this.airlineName = airlineName;
+      this.country = country;
+      this.city = city;
+      this.address = address;
+      this.identityColor = identityColor;
+      this.slogan = slogan;
+      this.flights = flights
+    }
+  }
