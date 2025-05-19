cad48d53b5cb48c00b7e5526a3bca51efd89e244
Felipe <f.sanmiguel@uniandes.edu.co>
2025-05-19 16:59:13 -0500

    Cargaron pero no es suficiente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
+import { HomeModule } from './Home/Home.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,6 +15,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HomeModule,
     HttpClientModule
   ],
   providers: [],
